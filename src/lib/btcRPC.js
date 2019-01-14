0d66b98fcb67f7ccdea152e874f09a4d1ed43876
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2019-01-14 16:48:00 -0700

    create btcRPC class for handling rpc method calls

---
+++ src/lib/btcRPC.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+class btcRPC {
+  static async query(method, params = []) {
+    const config = {
+      method: 'post',
+      url: process.env.BTC_RPC_URL,
+      headers: { 'content-type': 'text/plain' },
+      auth: {
+        username: process.env.BTC_RPC_USER,
+        password: process.env.BTC_RPC_PASSWORD,
+      },
+      data: JSON.stringify({
+        'jsonrpc': '2.0',
+        'id': method,
+        'method': method,
+        'params': params,
+      }),
+    }
+
+    try {
+      const result = await axios(config);
+      return result.data;
+    } catch (err) {
+      return err;
+    }
+  }
+}
+
+export default btcRPC;
