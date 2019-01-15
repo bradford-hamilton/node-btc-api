77f86eb5faf94ab94e0e2e9940e4f4cbd08c2ac1
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2019-01-14 19:41:38 -0700

    Add get block by block hash route

--- src/routes/index.js
+++ src/routes/index.js
@@ -19,4 +19,17 @@ router.get('/info', async (req, res, next) => {
   }
 });
 
+router.get('/block/:blockHash', async (req, res, next) => {
+  const method = 'getblock';
+  const verbosity = req.query.verbosity ? parseInt(req.query.verbosity, 10) : 1;
+  const params = [req.params.blockHash, verbosity];
+
+  try {
+    const result = await btcRPC.query(method, params);
+    res.json(result);
+  } catch (err) {
+    res.json({ message: `Error with RPC call ${method}: ${err}` });
+  }
+});
+
 export default router;
